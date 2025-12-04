fab5b92251711a796114d04c7e43f1c494cc81fe
LemonT8269 <2636956919@qq.com>
2025-12-04 19:48:01 +0800

    修改充气量ui使之与PlayerController同步

--- Assets/Scripts/UI/Barometer.cs
+++ Assets/Scripts/UI/Barometer.cs
@@ -5,63 +5,83 @@ using UnityEngine.UI;
 
 public class Barometer : MonoBehaviour
 {
+
     [Header("Pressure(%)")]
-    [SerializeField] private float maxPressure = 120f;
-    [SerializeField] private float currentPressure = 50f;
+    [SerializeField] private float maxPressure = 2.0f;
+    [SerializeField] private float currentPressure = 0.5f;
+    [SerializeField] private float targetPressure = 1.0f;
+
     [Header("Threshold")]
-    [SerializeField] private float threshold1 = 25f;
-    [SerializeField] private float threshold2 = 50f;
-    [SerializeField] private float threshold3 = 75f;
-    [SerializeField] private float threshold4 = 100f;
+    [SerializeField] private float flyThreshold = 1.2f; // 飞行阈值（这里的阈值均为下限）
+    [SerializeField] private float boostThreshold = 0.9f; // 加强阈值
+    [SerializeField] private float normalThreshold = 0.7f; // 正常阈值
+    [SerializeField] private float halfPowerThreshold = 0.5f; // 半功率阈值
+    [SerializeField] private float noJumpThreshold = 0.3f; // 无跳跃阈值
+
     [Header("UI")]
     [SerializeField] private Image barometerFillImage;
     [SerializeField] private TMPro.TextMeshProUGUI barometerText;
+
     [Header("Animation")]
     //[SerializeField] private Gradient barometerGradient;
     [SerializeField] private float smoothSpeed = 10f;
-    [Header("Debug")]
-    [SerializeField] private bool setTargetPressureAtStart = true;
-    [SerializeField] private float targetPressure=100f;
-    // Start is called before the first frame update
-    void Start()
+
+    void Awake()
     {
         if(barometerFillImage == null)
             barometerFillImage=GetComponent<Image>();
         if(barometerText == null)
             barometerText=GetComponentInChildren<TMPro.TextMeshProUGUI>();
-        if(setTargetPressureAtStart==true)
-            targetPressure = currentPressure;
+    }
+
+    void Start()
+    {
+        if (PlayerController.Instance != null)
+        {
+            targetPressure = PlayerController.Instance.inflationScale;
+            currentPressure = targetPressure;
+            //Debug.Log("target:"+targetPressure+" current:"+currentPressure);
+        }
+        //Debug.Log("target:" + targetPressure + " current:" + currentPressure);
         UpdateBarometer();
     }
 
-    // Update is called once per frame
     void Update()
     {
+        if (PlayerController.Instance != null)
+        {
+            targetPressure = PlayerController.Instance.inflationScale;
+        }
         if (Mathf.Abs(currentPressure - targetPressure) > 0.01f)
         {
             currentPressure=Mathf.Lerp(currentPressure, targetPressure, smoothSpeed*Time.deltaTime);
             UpdateBarometer();
         }
+        //Debug.Log("target:" + targetPressure + " current:" + currentPressure);
+
     }
+
     void UpdateBarometer()
     {
         if (barometerFillImage != null)
         {
             barometerFillImage.fillAmount = currentPressure / maxPressure;
-            if (currentPressure < threshold1)
+            if (currentPressure < noJumpThreshold)
                 barometerFillImage.color = Color.white;
-            else if (currentPressure < threshold2)
+            else if (currentPressure < halfPowerThreshold)
                 barometerFillImage.color = Color.green;
-            else if (currentPressure < threshold3)
+            else if (currentPressure < normalThreshold)
                 barometerFillImage.color = Color.yellow;
-            else if (currentPressure < threshold4)
+            else if (currentPressure < boostThreshold)
+                barometerFillImage.color = new Color(1f, 165f / 255f, 0);
+            else if (currentPressure < flyThreshold)
                 barometerFillImage.color = Color.red;
             else
                 barometerFillImage.color = Color.red;
         }
         if(barometerText != null)
         {
-            barometerText.text = Mathf.CeilToInt(currentPressure).ToString();
+            barometerText.text = Mathf.CeilToInt(currentPressure*100).ToString();
         }
     }
 }
